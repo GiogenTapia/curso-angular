e2607d9683a3931c8b0e5f4e6502e39f0ddcfb3d
Giovanni Tapia <ryukaj1_@hotmail.com>
2023-04-28 13:03:02 -0600

    implementacion de todos los hooks

--- lifeCycle/src/app/pages/pagina1/pagina1.component.ts
+++ lifeCycle/src/app/pages/pagina1/pagina1.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.component.html',
   styleUrls: ['./pagina1.component.css']
 })
-export class Pagina1Component {
+export class Pagina1Component implements OnInit, OnChanges, DoCheck,
+ AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+  
+  //Esto es para cuando el componente esta creado
+  //y podemos acceder al html
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  //En esta parte se realizan inyeccciones de dependencias.
+  constructor(){
+    console.log('constructor');
+    
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Method not implemented.');
+  }
+  ngDoCheck(): void {
+    console.log('Method not implemented.');
+  }
+  ngAfterContentInit(): void {
+   console.log('Method not implemented.');
+  }
+  ngAfterContentChecked(): void {
+    console.log('Method not implemented.');
+  }
+  ngAfterViewInit(): void {
+    console.log('Method not implemented.');
+  }
+  ngAfterViewChecked(): void {
+    console.log('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    console.log('Method not implemented.');
+  }
+
 
 }
