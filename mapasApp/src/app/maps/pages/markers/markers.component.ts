58b945b62dc213c6932a5e98f0184f66f4da7055
Giovanni Tapia <ryukaj1_@hotmail.com>
2023-05-25 23:46:21 -0600

    Agregado de zoom y diseño de marcadores

--- mapasApp/src/app/maps/pages/markers/markers.component.ts
+++ mapasApp/src/app/maps/pages/markers/markers.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from "mapbox-gl";
 
 @Component({
   selector: 'app-markers',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./markers.component.css']
 })
 export class MarkersComponent {
+
+
+  @ViewChild('map') divMap?:  ElementRef;
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-101.19911340522617, 19.70357998824808);
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'HTML element no found';
+
+     this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      });
+
+      /*
+      //Crear elementos HTML
+      const markerHtml = document.createElement('div');
+      markerHtml.innerHTML = 'Giovanni';
+      const marker = new Marker(
+        {
+          // color: 'red'
+          // element: markerHtml
+        }
+      )
+      .setLngLat(this.currentLngLat)
+      .addTo(this.map);
+
+*/
+  }
 
 }
--- mapasApp/src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ mapasApp/src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import{ Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import{ LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 
 
@@ -8,20 +8,62 @@ import{ Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
+
 
   @ViewChild('map') divMap?:  ElementRef;
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-101.19911340522617, 19.70357998824808);
 
   ngAfterViewInit(): void {
 
     if (!this.divMap) throw 'HTML element no found';
 
-    const map = new Map({
+     this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
       });
+
+      this.mapListeners();
+  }
+
+  //con ello se destruyen los listener que se crearon
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  mapListeners(){
+    if(!this.map) throw "Mapa no inicializado";
+
+    this.map.on('zoom', (ev)=>{
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev)=>{
+      if(this.map!.getZoom()< 18) return;
+      this.map!.zoomTo(18);
+    });
+    this.map.on('move' ,()=>{
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat} = this.currentLngLat;
+    });
+
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value : string ){
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 
 }
