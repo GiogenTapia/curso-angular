e206dc47bdaefb02fe5cca87025d25afe4a8253e
Giovanni Tapia <ryukaj1_@hotmail.com>
2023-05-26 01:03:25 -0600

    Terminacion de properties en mapas

--- mapasApp/src/app/maps/components/min-map/min-map.component.ts
+++ mapasApp/src/app/maps/components/min-map/min-map.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from "mapbox-gl";
 @Component({
-  selector: 'app-min-map',
+  selector: 'map-min-map',
   templateUrl: './min-map.component.html',
   styleUrls: ['./min-map.component.css']
 })
-export class MinMapComponent {
+export class MinMapComponent implements AfterViewInit{
+
+  @Input() lngLat?: [number,number];
+  @ViewChild('map') divMap?:  ElementRef;
+
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+
+   if(!this.divMap?.nativeElement) throw "Map Div not found";
+   if(!this.lngLat) throw "LngLat can't be null";
+
+   this.map = new Map({
+    container: this.divMap?.nativeElement,
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: this.lngLat, // starting position [lng, lat]
+    zoom: 15,
+    interactive: false
+   });
+
+   new Marker()
+  .setLngLat(this.lngLat)
+  .addTo(this.map);
+
+
+
+  }
+
+
+
+
+
+
 
 }
