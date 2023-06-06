b9de86e490323ee4a0bb3bf4646d325af58150a7
GiogenTapia <ryukaj1_@hotmail.com>
2023-06-05 21:19:00 -0600

    agregando nuevo proyecto

---
+++ directiveSignalsApp/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
