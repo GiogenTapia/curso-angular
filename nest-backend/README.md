34f399bd8ccb66032abe7b0afbdff2e2c20228e7
Giovanni Tapia <ryukaj1_@hotmail.com>
2023-06-14 13:33:52 -0600

    agregando proyecto backend

---
+++ nest-backend/README.md
@@ -0,0 +1,8 @@
+# Backend en Nest
+
+...
+
+docker compose uo -d
+...
+
+copiar el ```.env.tempalte``` y renombrarlo en ```.env```
---
+++ nest-backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigModule } from '@nestjs/config';
+import { log } from 'console';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI)],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+
+  constructor(){
+ 
+    
+  }
+
+}
