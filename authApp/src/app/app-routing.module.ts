c1f98e167269bbcaa69eee520be5a825e18c5e3e
Giovanni Tapia <ryukaj1_@hotmail.com>
2023-08-12 00:10:43 -0600

    agregando diseño login

--- authApp/src/app/app-routing.module.ts
+++ authApp/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'auth',
+    // guards
+    loadChildren: ()=> import ('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path:'dashboard',
+    // guards
+    loadChildren: ()=> import ('./dashboard/dashboard.module').then(m => m.DashboardModule),
+  },
+
+  {
+    path:'**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
