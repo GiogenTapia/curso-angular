798d8437c8cc48cd4a152d08d2bd7dee35f52b18
GiogenTapia <tapiamgio@gmail.com>
2024-08-08 18:27:36 -0600

    Agregando Defer Blocks

--- my-dashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ my-dashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeavyLoadersSlowComponent],
   templateUrl: './defer-views.component.html',
-  styles: ``
 })
 export default class DeferViewsComponent {
 
--- my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template:`
-  <h1> HOLA MUNDO </h1>
+  <section [ngClass]="['w-full h-[600px]', cssClass]">
+    Heavy Loader Slow
+  </section>
   `
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({required : true}) cssClass!: string;
+
+
+  /**
+   *
+   */
+  constructor() {
+
+    const start = Date.now();
+    while(Date.now() - start < 3000){
+      console.log('Cargado');
+
+    }
+
+  }
+
+
+
+
 
 }
